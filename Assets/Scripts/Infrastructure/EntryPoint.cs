f35aa5b1b227a2970a4349c9211af97926ee2256
Gayduk Alexey <gaayduk@gmail.com>
2023-12-06 12:20:24 +0200

    Init

---
+++ Assets/Scripts/Infrastructure/EntryPoint.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Infrastructure
+{
+    public class EntryPoint : MonoBehaviour
+    {
+        [SerializeField] private Enemy[] _enemies;
+        [SerializeField] private Weapon[] _weapons;
+
+        private void Awake()
+        {
+            
+        }
+    }
+
+    public class Enemy : ScriptableObject
+    { }
+
+    public abstract class Weapon : MonoBehaviour
+    { }
+
+    public class Firearm : Weapon
+    {
+    }
+
+    public class Melee : Weapon
+    {
+    }
+
+    public class Factory<T>
+    {
+    
+    }
+
+    public class Pool<T>
+    {
+        
+    }
+}
