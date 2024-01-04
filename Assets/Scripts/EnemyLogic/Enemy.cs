2761e1fff1efd553089b7ab16b648d8b76643500
Gayduk Alexey <gaayduk@gmail.com>
2024-01-04 21:27:11 +0200

    Floating text

--- Assets/Scripts/EnemyLogic/Enemy.cs
+++ Assets/Scripts/EnemyLogic/Enemy.cs
@@ -1,6 +1,7 @@
 ﻿using Assets.Scripts.Infrastructure;
 using MoreMountains.Feedbacks;
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Assets.Scripts.EnemyLogic
@@ -20,6 +21,7 @@ namespace Assets.Scripts.EnemyLogic
         [field: SerializeField] public SpriteRenderer SpriteRenderer { get; private set; }
         
         private MMF_Player _onDamagePlayer;
+        private MMF_FloatingText _onDamageFloatingText;
 
         public void Construct(MMF_Player onDamagePlayer)
         {
@@ -30,25 +32,22 @@ namespace Assets.Scripts.EnemyLogic
         public void Initialize()
         {
             _onDamagePlayer.GetFeedbackOfType<MMF_Scale>().AnimateScaleTarget = transform;
+            _onDamageFloatingText = _onDamagePlayer.GetFeedbackOfType<MMF_FloatingText>();
             _onDamagePlayer.GetFeedbackOfType<MMF_Flicker>().BoundRenderer = GetComponent<SpriteRenderer>();
-            _onDamagePlayer.enabled = true;
             _onDamagePlayer.Initialization();
-
-         //   _onDamagePlayer.SetCanPlay(true);
+            _onDamagePlayer.SetCanPlay(true);
         }
 
         public void TakeDamage(int damage)
         {
             var prevLife = ActualHp;
-            
             ActualHp -= damage;
-
             var earnedMoneys = Mathf.Abs(prevLife - Mathf.Max(ActualHp,0));
-
-            OnDamageTake?.Invoke(ActualHp, MaxHp);
-            
             ServiceLocator.GetService<Player>().AddMoney(earnedMoneys);
+            
+            OnDamageTake?.Invoke(ActualHp, MaxHp);
 
+            _onDamageFloatingText.Value = damage.ToString();
             _onDamagePlayer.PlayFeedbacks();
 
             CheckHP();
@@ -59,8 +58,8 @@ namespace Assets.Scripts.EnemyLogic
             if (ActualHp <= 0)
             {
                 _onDamagePlayer.StopFeedbacks();
-                _onDamagePlayer.enabled = false;
-              //  _onDamagePlayer.SetCanPlay(false); 
+                _onDamagePlayer.SetCanPlay(false);
+                _onDamagePlayer.RestoreInitialValues();
 
                 OnDie?.Invoke();
                 AllowToAttack = false;
