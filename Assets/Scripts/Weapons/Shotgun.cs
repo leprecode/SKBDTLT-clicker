d38cd72b57333b11ec39326a77ef9d589abe991c
Gayduk Alexey <gaayduk@gmail.com>
2024-01-10 02:35:44 +0200

    Shotgun

--- Assets/Scripts/Weapons/Shotgun.cs
+++ Assets/Scripts/Weapons/Shotgun.cs
@@ -7,12 +7,12 @@ using UnityEngine;
 
 namespace Assets.Scripts.Weapons
 {
-    public class Pistol : Weapon
+    public class Shotgun : Weapon
     {
         private readonly Color32 RESET_COLOR = new Color32(255, 255, 255, 255);
         [SerializeField] private SpriteRenderer _spriteRenderer;
-        [SerializeField] private MMF_Player _onPistolFire;
-        [SerializeField] private Transform _muzzleFlash;
+        [SerializeField] private MMF_Player _onFire;
+        [SerializeField] private float _offsetBeforeFading;
         private MMF_Player _onBulletHit;
         private MMF_ParticlesInstantiation _particlesInstantiationOnBulletHit;
         private WeaponModel _pool;
@@ -30,7 +30,6 @@ namespace Assets.Scripts.Weapons
             transform.eulerAngles = Vector3.zero;
             transform.position = Vector3.zero;
             transform.localScale = new Vector3(transform.localScale.x, Mathf.Abs(transform.localScale.y), transform.localScale.z);
-            _muzzleFlash.localScale = new Vector3(_muzzleFlash.localScale.x, Mathf.Abs(_muzzleFlash.localScale.y), _muzzleFlash.localScale.z);
         }
 
         public override void Attack(Vector3 position, Enemy enemy)
@@ -38,22 +37,17 @@ namespace Assets.Scripts.Weapons
             transform.position = GetRandomPosition(transform.position);
             RotateToTarget(enemy);
 
-            float lastAttackTime = 0;
-
-            for (int i = 0; i < ShotsCount; i++) 
+            for (int i = 0; i < ShotsCount; i++)
             {
-                var timeToNextShot = SecondsToOneShot * i;
-                lastAttackTime = timeToNextShot;
-                StartCoroutine(Shoot(timeToNextShot, GetRandomPosition (position), enemy));
+                Shoot(GetRandomPosition(position), enemy);
             }
 
-            StartCoroutine(OnEndAttack(lastAttackTime));
+            StartCoroutine(OnEndAttack());
         }
 
-        private IEnumerator Shoot(float seconds, Vector3 pos, Enemy enemy)
+        private void Shoot(Vector3 pos, Enemy enemy)
         {
-            yield return new WaitForSeconds(seconds);
-            _onPistolFire.PlayFeedbacks();
+            _onFire.PlayFeedbacks();
 
             _particlesInstantiationOnBulletHit.TargetWorldPosition = pos;
             _onBulletHit.PlayFeedbacks();
@@ -64,30 +58,24 @@ namespace Assets.Scripts.Weapons
         private void RotateToTarget(Enemy enemy)
         {
             if (transform.position.x < enemy.transform.position.x)
-            {
                 transform.localScale = new Vector3(transform.localScale.x, Mathf.Abs(transform.localScale.y), transform.localScale.z);
-                _muzzleFlash.localScale = new Vector3(_muzzleFlash.localScale.x, Mathf.Abs(_muzzleFlash.localScale.y), _muzzleFlash.localScale.z);
-            }
             else
-            {
                 transform.localScale = new Vector3(transform.localScale.x, Mathf.Abs(transform.localScale.y) * -1, transform.localScale.z);
-                _muzzleFlash.localScale = new Vector3(_muzzleFlash.localScale.x, Mathf.Abs(_muzzleFlash.localScale.y) * -1, _muzzleFlash.localScale.z);
-            }
-
 
             Vector3 direction = enemy.transform.position - transform.position;
             float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
             transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
         }
 
-
         private Vector3 GetRandomPosition(Vector3 startPos)
         {
             return new Vector3(startPos.x + Random.Range(-0.5f, 1), startPos.y + Random.Range(-0.5f, 1), 0);
         }
-        private IEnumerator OnEndAttack(float timeOffset)
+
+        private IEnumerator OnEndAttack()
         {
-            yield return new WaitForSeconds(timeOffset);
+            yield return new WaitForSeconds(_offsetBeforeFading);
+
             _spriteRenderer.DOFade(0f, FadeDuration).OnComplete(BackToPool);
         }
 
